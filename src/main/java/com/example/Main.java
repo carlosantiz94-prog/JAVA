d48aa7e30791d6114afaee870c931d3c56bdbf85
carlosantiz94-prog <carlosantiz94@gmail.com>
2026-02-27 18:30:20 -0500

    Solucion a los ejercicios de POO

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,35 +1,38 @@
 package com.example;
 
 public class Main {
-    public static void main(String[] args) {
-        // Prueba 1: Usando el constructor vacío
-        Libro l1 = new Libro(); 
-        l1.mostrarDetalles(); 
 
-        // Prueba 2: Usando título y autor
-        Libro l2 = new Libro("Cancion de Hielo y Fuego", "George R.R. Martin");
+    public static void main(String[] args) {
+
+        // Constructor vacío
+        Libro l1 = new Libro();
+        l1.mostrarDetalles();
+
+        // Constructor con título y autor
+        Libro l2 = new Libro("Harry Potter", "J.K. Rowling");
         l2.mostrarDetalles();
 
-        // Prueba 3: Usando el constructor completo
+        // Constructor completo
         Libro l3 = new Libro("El Señor de los Anillos", "J.R.R. Tolkien", 1200);
         l3.mostrarDetalles();
-         
-        System.out.println("--------------------------------------------------\n");
 
-        //ejercicio2
 
-        CuentaBancaria cuenta = new CuentaBancaria("Maria", 500);
+        System.out.println("-------------");
+        
+
+        //* ejercicio2 */
+        CuentaBancaria cuenta = new CuentaBancaria("camilo", 500);
         cuenta.depositar(200);
         System.out.println(cuenta.getSaldo()); // Debería ser 700
         cuenta.retirar(1000); // Debería mostrar error de fondos insuficiente
 
-         System.out.println("--------------------------------------------------\n");
 
-        //ejercicio3
+        System.out.println("-------------");
 
-        // En Main.java
+
+        //* ejercicio 3 */
         Estudiante e = new Estudiante("Pedro", 21, 2.5);
-        e.mostrarInfo(); // Debería indicar REPROBADO
+e.mostrarInfo(); // Debería indicar REPROBADO
+
     }
-    
 }
